cc1d6d20004566267e5e5db431c488ac0c8eb659
trxsh <89441166+trxshonzaza@users.noreply.github.com>
2025-08-05 01:51:20 -0400

    added more loop functions

--- src/main/java/trxsh/ontop/theUltimateSMPLib/other/Loops.java
+++ src/main/java/trxsh/ontop/theUltimateSMPLib/other/Loops.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 
 public class Loops {
     private static final Map<String, BukkitRunnable> loops = new HashMap<>();
@@ -19,6 +20,55 @@ public class Loops {
         }
 
         loops.put(key, task);
+    }
+
+    public static void loopUntil(Runnable task, int delay, Supplier<Boolean> condition) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if(condition.get()) {
+                    cancel();
+                    return;
+                }
+
+                task.run();
+            }
+        }.runTaskTimer(Main.getInstance(), 0, delay);
+    }
+
+    public static void loopForTicks(Runnable task, int delay, int maxTicks) {
+        new BukkitRunnable() {
+            int ticks = 0;
+
+            @Override
+            public void run() {
+                if(ticks >= maxTicks) {
+                    cancel();
+                    return;
+                }
+
+                task.run();
+                ticks++;
+            }
+        }.runTaskTimer(Main.getInstance(), 0, delay);
+    }
+
+    public static void loopForTicks(Runnable task, Runnable onComplete, int delay, int maxTicks) {
+        new BukkitRunnable() {
+            int ticks = 0;
+
+            @Override
+            public void run() {
+                if(ticks >= maxTicks) {
+                    cancel();
+                    onComplete.run();
+                    return;
+                }
+
+                task.run();
+                ticks++;
+            }
+        }.runTaskTimer(Main.getInstance(), 0, delay);
     }
 
     public static void endLoop(String key) {
